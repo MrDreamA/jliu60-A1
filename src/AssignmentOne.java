f9a3ddc96b62d2aff7295ae9e242485b5615e4d5
Mr.Dream <146167041+MrDreamA@users.noreply.github.com>
2024-11-12 17:22:35 +0800

    1

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -5,11 +5,11 @@ public class AssignmentOne {
         // Part 3 - Using classes and objects
 
         // Create instances of the classes
-        HealthProfessional generalPractitioner1 = new GeneralPractitioner(1, "Kate", "Male", "General Practitioner", "MBBS");
-        HealthProfessional generalPractitioner2 = new GeneralPractitioner(2, "Lily", "Female", "General Practitioner", "MBBS");
-        HealthProfessional generalPractitioner3 = new GeneralPractitioner(3, "John", "Male", "General Practitioner", "MBBS");
-        HealthProfessional nursePractitioner1 = new NursePractitioner(4, "Jane", "Female", "Nurse Practitioner", "RN");
-        HealthProfessional nursePractitioner2 = new NursePractitioner(5, "Adam", "Male", "Nurse Practitioner", "RN");
+        HealthProfessional generalPractitioner1 = new GeneralPractitioner(1, "Kunye", "Male", "General Practitioner", "MBBS");
+        HealthProfessional generalPractitioner2 = new GeneralPractitioner(2, "YunlinLi", "Female", "General Practitioner", "MBBS");
+        HealthProfessional generalPractitioner3 = new GeneralPractitioner(3, "YunhengTan", "Male", "General Practitioner", "MBBS");
+        HealthProfessional nursePractitioner1 = new NursePractitioner(4, "Yeyan", "Female", "Nurse Practitioner", "RN");
+        HealthProfessional nursePractitioner2 = new NursePractitioner(5, "ZihanWei", "Male", "Nurse Practitioner", "RN");
         // Print the details of the health professionals
         generalPractitioner1.printAllVariables();
         generalPractitioner2.printAllVariables();
