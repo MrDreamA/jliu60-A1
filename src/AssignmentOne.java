811819603130f063d8b51e7ea24585e2920290cc
Junyi <2321046848@qq.com>
2024-11-12 13:21:21 +0800

    Add Part 3

--- src/AssignmentOne.java
+++ src/AssignmentOne.java
@@ -1,5 +1,19 @@
 public class AssignmentOne {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        // Part 3 - Using classes and objects
+
+        // Create instances of the classes
+        HealthProfessional generalPractitioner1 = new GeneralPractitioner(1, "Kate", "Male", "General Practitioner", "MBBS");
+        HealthProfessional generalPractitioner2 = new GeneralPractitioner(2, "Lily", "Female", "General Practitioner", "MBBS");
+        HealthProfessional generalPractitioner3 = new GeneralPractitioner(3, "John", "Male", "General Practitioner", "MBBS");
+        HealthProfessional nursePractitioner1 = new NursePractitioner(4, "Jane", "Female", "Nurse Practitioner", "RN");
+        HealthProfessional nursePractitioner2 = new NursePractitioner(5, "Adam", "Male", "Nurse Practitioner", "RN");
+        // Print the details of the health professionals
+        generalPractitioner1.printAllVariables();
+        generalPractitioner2.printAllVariables();
+        generalPractitioner3.printAllVariables();
+        nursePractitioner1.printAllVariables();
+        nursePractitioner2.printAllVariables();
+        System.out.println("------------------------------");
     }
 }
